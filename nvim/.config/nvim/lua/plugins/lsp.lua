c01f7e1a59d22243b39d521c771c1f862a5a12cb
hokix <hokix@live.com>
2024-03-12 14:57:38 +0800

    cleanup

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -298,35 +298,6 @@ local M = {
 		ft = { "go", "gomod" },
 		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	},
-	-- {
-	-- 	"jay-babu/mason-nvim-dap.nvim",
-	-- 	dependencies = "mason.nvim",
-	-- 	cmd = { "DapInstall", "DapUninstall" },
-	-- 	opts = {
-	-- 		-- Makes a best effort to setup the various debuggers with
-	-- 		-- reasonable debug configurations
-	-- 		automatic_installation = true,
-	--
-	-- 		-- You can provide additional configuration to the handlers,
-	-- 		-- see mason-nvim-dap README for more information
-	-- 		handlers = {
-	-- 			function(config)
-	-- 				-- all sources with no handler get passed here
-	--
-	-- 				-- Keep original functionality
-	-- 				require("mason-nvim-dap").default_setup(config)
-	-- 			end,
-	-- 		},
-	--
-	-- 		-- You'll need to check that you have the required things installed
-	-- 		-- online, please don't ask me how to install them :)
-	-- 		ensure_installed = {
-	-- 			-- Update this to ensure that you have the debuggers for the langs you want
-	-- 			"delve",
-	-- 			"cppdbg",
-	-- 		},
-	-- 	},
-	-- },
 	{
 		"nvimdev/lspsaga.nvim",
 		-- event = "LspAttach",
