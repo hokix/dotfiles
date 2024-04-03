6bc36c7e2d621c6580e651665fd06f3a9de950cd
hokix <hokix@live.com>
2024-04-03 14:10:37 +0800

    neovim c++ codelldb & cpptools

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -26,45 +26,46 @@ return {
         }
       end
       for _, lang in ipairs({ "c", "cpp" }) do
-        dap.configurations[lang] = {
-          {
-            type = "cppdbg",
-            request = "launch",
-            name = "Launch file",
-            program = function()
-              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-            end,
-            cwd = function()
-              return vim.fn.input("Path to cwd: ", vim.fn.getcwd() .. "/", "file")
-            end,
-            MIMode = "gdb",
-            miDebuggerPath = "/usr/bin/gdb",
-            setupCommands = {
+        if type(dap.configurations[lang]) == "table" then
+          vim.list_extend(dap.configurations[lang], {
+            {
+              type = "cppdbg",
+              request = "launch",
+              name = "Launch file[cpptools]",
+              program = function()
+                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              cwd = function()
+                return vim.fn.input("Path to cwd: ", vim.fn.getcwd() .. "/", "file")
+              end,
+              MIMode = "gdb",
+              miDebuggerPath = "/usr/bin/gdb",
+              setupCommands = {
                 {
-                    description = "Enable pretty-printing for gdb",
-                    text= "-enable-pretty-printing",
-                    ignoreFailures = true
+                  description = "Enable pretty-printing for gdb",
+                  text = "-enable-pretty-printing",
+                  ignoreFailures = true,
                 },
                 {
-                    description = "Enable follow child for gdb",
-                    text= "-gdb-set follow-fork-mode child",
-                    ignoreFailures = true
-                }
-            }
-          },
-          {
-            type = "cppdbg",
-            request = "attach",
-            name = "Attach to process",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-        }
+                  description = "Enable follow child for gdb",
+                  text = "-gdb-set follow-fork-mode child",
+                  ignoreFailures = true,
+                },
+              },
+            },
+            {
+              type = "cppdbg",
+              request = "attach",
+              name = "Attach to process[cpptools]",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
+            },
+          })
+        end
       end
 
       -- setup dap config by VsCode launch.json file
       require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp" } })
-
-    end
+    end,
   },
 }
