c84eb1508c320b88a376a686302a253309aa9d4b
hokix <hokix@live.com>
2024-04-09 17:28:46 +0800

    nvim go formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,20 @@
 return {
   {
     "stevearc/conform.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        if type(opts.ensure_installed) == "table" then
+          vim.list_extend(opts.ensure_installed, { "clang-format" })
+        end
+      end,
+    },
     opts = {
       formatters_by_ft = {
-        c = {"clang-format"},
-        cpp = {"clang-format"},
-      }
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        go = { "goimports", "gofmt" },
+      },
     },
   },
 }
