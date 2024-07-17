3b5f217ab4be9ffa88fd5ac474af40a362161b27
hokix <hokix@live.com>
2024-07-17 19:14:42 +0800

    neotest-gtest

--- nvim/.config/nvim/lua/plugins/cpp.lua
+++ nvim/.config/nvim/lua/plugins/cpp.lua
@@ -17,4 +17,18 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "alfaix/neotest-gtest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-gtest"] = {
+          debug_adapter = "codelldb",
+        },
+      },
+    },
+  },
 }
