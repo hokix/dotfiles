b136201f5e3f0d37ff26f673eed89a6388cdf79c
hokix <hokix@live.com>
2024-10-31 17:34:19 +0800

    remove clang-format

--- nvim/.config/nvim/lua/plugins/cpp.lua
+++ nvim/.config/nvim/lua/plugins/cpp.lua
@@ -1,23 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    optional = true,
-    dependencies = {
-      "williamboman/mason.nvim",
-      opts = function(_, opts)
-        if type(opts.ensure_installed) == "table" then
-          vim.list_extend(opts.ensure_installed, { "clang-format" })
-        end
-      end,
-    },
-    opts = {
-      formatters_by_ft = {
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-      },
-    },
-  },
-  {
     "nvim-neotest/neotest",
     optional = true,
     dependencies = {
