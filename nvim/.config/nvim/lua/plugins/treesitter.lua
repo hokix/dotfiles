82d412cb64adb256347ad2027d6dddf49213d498
hokix <hokix@live.com>
2025-01-07 19:05:00 +0800

    treesitter compilers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,5 +3,12 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
+    opts = function()
+      local install = require 'nvim-treesitter.install'
+      vim.list_extend(install.compilers, {
+        "~/.local/extra/bin/gcc",
+        "~/.local/extra/bin/g++",
+      })
+    end
   },
 }
