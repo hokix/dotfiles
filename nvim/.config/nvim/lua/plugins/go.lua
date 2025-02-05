f1d968eb6812d75f9c6e00cab7fe92bc4b90b715
hokix <hokix@live.com>
2025-02-05 19:52:19 +0800

    update nvim

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -85,7 +85,7 @@ return {
     dependencies = {
       {
         "williamboman/mason.nvim",
-                opts = { ensure_installed = { "gomodifytags", "impl" } },
+        opts = { ensure_installed = { "gomodifytags", "impl" } },
       },
     },
     opts = function(_, opts)
@@ -93,8 +93,8 @@ return {
       opts.sources = vim.list_extend(opts.sources or {}, {
         nls.builtins.code_actions.gomodifytags,
         nls.builtins.code_actions.impl,
-        nls.builtins.formatting.goimports,
-        nls.builtins.formatting.gofmt,
+        -- nls.builtins.formatting.goimports,
+        -- nls.builtins.formatting.gofmt,
       })
     end,
   },
--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,31 +1,36 @@
 return {
-  {
     {
-      "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-      lazy = true,
-      event = "VeryLazy",
+        {
+            "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+            lazy = true,
+            event = "VeryLazy",
+        },
+        {
+            "tpope/vim-abolish", -- Abbreviation,
+            lazy = true,
+            init = function()
+                vim.g.abolish_no_mappings = true
+            end,
+            cmd = {
+                "Abolish",
+                "Subvert",
+            },
+            keys = {
+                { "co", "<Plug>(abolish-coerce-word)", desc = "Abolish [co]erce" },
+            },
+        },
     },
     {
-      "tpope/vim-abolish", -- Abbreviation,
-      lazy = true,
-      init = function()
-        vim.g.abolish_no_mappings = true
-      end,
-      cmd = {
-        "Abolish",
-        "Subvert",
-      },
-      keys = {
-        { "co", "<Plug>(abolish-coerce-word)", desc = "Abolish [co]erce" },
-      },
+        "skywind3000/asyncrun.vim",
+        lazy = true,
+        cmd = { "AsyncRun" },
+        init = function()
+            vim.g.asyncrun_open = 10
+        end,
     },
-  },
-  {
-    "skywind3000/asyncrun.vim",
-    lazy = true,
-    cmd = { "AsyncRun" },
-    init = function()
-      vim.g.asyncrun_open = 10
-    end,
-  },
+    -- {
+    --     "ibhagwan/fzf-lua",
+    --     optional = true,
+    --     commit = "39de20b42285c6a96a47e92703b2dacc55da835f",
+    -- }
 }
