3984ec3c4934f7b685da55e06ef1029094ea3510
hokix <hokix@live.com>
2026-02-25 10:13:28 +0800

    feat: update ai configs

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -6,11 +6,12 @@ return {
     },
     -- uncomment the following line to load hub lazily
     cmd = "MCPHub", -- lazy load
-    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+    -- build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
     -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-    -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+    build = "bundled_build.lua", -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
     config = function()
       require("mcphub").setup({
+        use_bundled_binary = true,
         extensions = {
           avante = {
             make_slash_commands = true, -- make /slash commands from MCP server prompts
@@ -323,6 +324,10 @@ return {
           mux = {
             enabled = true,
             backend = "tmux",
+          },
+          ---@type table<string, sidekick.cli.Config|{}>
+          tools = {
+            copilot = { cmd = { "copilot", "--no-auto-update" } },
           },
         },
       },
